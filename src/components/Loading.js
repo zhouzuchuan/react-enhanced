7412f42ece0dc7a125e20864d1ca3eacae1030d1
fezzc <zhouzuchuan@icloud.com>
2019-03-05 13:08:08 +0800

    :arrow_up: 更新model-redux

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -11,12 +11,6 @@ import { RE_LOADING_NAME } from '../const'
 
 import './loading.css'
 
-/**
- * Loading 组件
- * include/exclude (string/string) 默认为请求全触发，设置值且值为字符串, 转成数组，判断request中间层的函数名是否在数组中 有则触发
- *
- * */
-
 const returnArray = (v, s = {}) => (isArray(v) ? v : [v]).some(o => s[o])
 
 const returnUpdate = (include, exclude, store) => {
@@ -26,7 +20,11 @@ const returnUpdate = (include, exclude, store) => {
         return returnArray(include, store)
     }
 }
-
+/**
+ * Loading 组件
+ * include/exclude (string/string) 默认为请求全触发，设置值且值为字符串, 转成数组，判断request中间层的函数名是否在数组中 有则触发
+ *
+ * */
 class Loading extends React.Component {
     shouldComponentUpdate(np) {
         return !isEqual(np, this.props)
@@ -41,7 +39,7 @@ class Loading extends React.Component {
             className = '',
             cover = false,
             content,
-            spin = {}
+            spin = {},
         } = this.props
 
         const RequestLoading = RE.RequestLoading
@@ -49,7 +47,7 @@ class Loading extends React.Component {
             <div className="RE_loading">
                 <div
                     className={classnames({
-                        [className]: className
+                        [className]: className,
                     })}
                 >
                     {null}
@@ -63,7 +61,7 @@ class Loading extends React.Component {
             <div
                 className={classnames('RE_loading_wrap', {
                     RE_update: update,
-                    [wrapClassName]: wrapClassName
+                    [wrapClassName]: wrapClassName,
                 })}
                 {...wrapStyle}
             >
@@ -79,7 +77,7 @@ class Loading extends React.Component {
 }
 Loading.propTypes = ['include', 'exclude'].reduce(
     (r, v) => ({ ...r, [v]: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]) }),
-    { content: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.element), PropTypes.element]) }
+    { content: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.element), PropTypes.element]) },
 )
 
 export default connect(
@@ -90,7 +88,7 @@ export default connect(
     (loadingStore, action, { include = null, exclude = null, ...rest }) => {
         return {
             ...rest,
-            update: returnUpdate(include, exclude, loadingStore)
+            update: returnUpdate(include, exclude, loadingStore),
         }
-    }
+    },
 )(Loading)
