e0ce24cc8503472813fe6cdd057d8183b8d5a96f
fezzc <zhouzuchuan@icloud.com>
2019-06-28 15:00:07 +0800

    fix: :bug: 修复typescript > 3.4 路由问题

--- src/components/Route.js
+++ src/components/Route.js
@@ -2,39 +2,11 @@ import * as React from 'react'
 import { Route } from 'react-router-dom'
 import RE from '../store'
 
-// 继承指定class
-export const extendsClass = A => class extends A {}
-
-export default class RcRoute extends React.Component {
-    componentRender = (mothed, ...params) => {
-        if (RE.guardFunction && !RE.guardFunction(params[0], RE.dispatch)) {
-            return null
-        }
-
-        if (typeof mothed === 'function') {
-            if (mothed.name === 'LoadableComponent') {
-                const CustComponent = extendsClass(mothed)
-                return new CustComponent()
-            } else {
-                // @ts-ignore
-                return new mothed(...params)
-            }
-        } else {
-            return mothed
-        }
+const RcRoute = props => {
+    if (RE.guardFunction && !RE.guardFunction(props, RE.dispatch)) {
+        return null
     }
-
-    render() {
-        return (
-            <Route
-                {...['component', 'render'].reduce(
-                    (r, v) => ({
-                        ...r,
-                        ...(r[v] && { [v]: (...a) => this.componentRender(r[v], ...a) }),
-                    }),
-                    this.props,
-                )}
-            />
-        )
-    }
+    return <Route {...props} />
 }
+
+export default RcRoute
